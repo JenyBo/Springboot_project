8bb0098f4c329d4c61e514141e94e2526a4006e5
jenybo <quangtruongyb2002@gmail.com>
2023-04-03 11:39:55 +0700

    update get method

---
+++ src/main/java/com/jenybo_project/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.jenybo_project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+
+}
--- src/main/java/com/jenybo_project/Main.java
+++ src/main/java/com/jenybo_project/Main.java
@@ -1,19 +1,81 @@
 package com.jenybo_project;
 
+import java.util.List;
+import java.util.Objects;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+    public Main(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
     }
 
-    @GetMapping("/greet")
-    public String greet(){
-        return "Hello";
+    @GetMapping
+    public List<Customer> getCustomer(){
+        return customerRepository.findAll();
     }
+
+//    @GetMapping("/greet")
+//    public GreetResponse greet(){
+//
+//        return new GreetResponse(
+//            "Hello",
+//            List.of("Java","Js","Python"),
+//            new Person("Truong",20,30.001));
+//    }
+//
+//    record Person(String name , int age , double savings){
+//
+//    }
+//    record GreetResponse(
+//        String greet,
+//        List<String> favProgram,
+//        Person person
+//    ){}
+//    class GreetResponse {
+//
+//    private final String greet;
+//
+//    GreetResponse(String greet) {
+//        this.greet = greet;
+//    }
+//
+//    public String getGreet() {
+//        return greet;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "GreetRespone{" +
+//            "greet='" + greet + '\'' +
+//            '}';
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) {
+//            return true;
+//        }
+//        if (o == null || getClass() != o.getClass()) {
+//            return false;
+//        }
+//        GreetResponse that = (GreetResponse) o;
+//        return Objects.equals(greet, that.greet);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(greet);
+//    }
+//    }
 }
