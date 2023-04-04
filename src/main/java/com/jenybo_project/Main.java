6cbb81f36169ef2a546dbf09a6a5150cb460466b
jenybo <quangtruongyb2002@gmail.com>
2023-04-04 14:49:38 +0700

    update full crud method

--- src/main/java/com/jenybo_project/Main.java
+++ src/main/java/com/jenybo_project/Main.java
@@ -4,7 +4,12 @@ import java.util.List;
 import java.util.Objects;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +29,30 @@ public class Main {
     public List<Customer> getCustomer(){
         return customerRepository.findAll();
     }
+    record NewCustomerRequest(String name, String email, Integer age) {
 
+    }
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id ,
+        @RequestBody NewCustomerRequest request){
+        Customer customer = customerRepository.findById(id).get();
+        customer.setName(request.name);
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
 //    @GetMapping("/greet")
 //    public GreetResponse greet(){
 //
